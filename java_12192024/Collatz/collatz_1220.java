f2492e069155e0dd0b7ffd15d2066d0b30f7c5fd
y-or-kshire <cslunate@gmail.com>
2024-12-20 14:45:00 +0900

    collatz problem
    i have few idea on Scanner class

---
+++ java_12192024/Collatz/collatz_1220.java
@@ -0,0 +1,26 @@
+package Collatz;
+
+import java.util.Scanner;
+
+class Collatz {
+    public static void main(String[] args) {
+
+        try (Scanner sc = new Scanner(System.in)) {
+        
+            int m = sc.nextInt();
+
+            int t = 0;
+            for(long n = m; n > 1; t++) {
+                if (n % 2 == 0) {
+                        n /= 2;
+                        System.out.println(n);
+                } else {
+                                n = n * 3 + 1;
+                        System.out.println(n);
+                };
+            };
+
+            System.out.println("\n" + t);
+        }
+    }
+}
